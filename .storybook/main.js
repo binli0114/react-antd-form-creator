45637ccc8c68c76780432e95d21440eada0c34e2
Bin Li <bin.li@genesys.com>
2024-04-21 10:46:46 +1000

    fix: migrate react 18 to fix some dependency issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,11 @@
 const config = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: [
+        "@storybook/addon-webpack5-compiler-swc",
+        "@storybook/addon-onboarding",
         "@storybook/addon-links",
         "@storybook/addon-essentials",
-        "@storybook/addon-onboarding",
+        "@chromatic-com/storybook",
         "@storybook/addon-interactions"
     ],
     framework: {
